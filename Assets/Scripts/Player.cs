4ba298971419a447131b3dc38228d1f4cf7cec5e
Blu <blu.social.contact@gmail.com>
2021-01-10 12:30:55 -0500

    hi?

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public int velocity;
+    public string[] items;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+
+        if(x != 0 || y!= 0)
+        {
+            transform.Translate(x * Time.deltaTime * velocity, y * Time.deltaTime * velocity , 0);
+        }
+
+        if (Input.GetKey("space"))
+        {
+            Debug.Log("Something Happened! You pressed the spacebar.");
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        
+
+    }
+}
